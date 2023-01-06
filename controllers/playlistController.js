6aab97f1f1d5ad2ad4b59c8eafdcf8fd30a6dadf
22PHOP-2D-Amarsaikhan <22phop-2d-amarsaikhan@LEAP-20s-iMac.local>
2023-01-06 16:09:45 +0800

    fourth

--- controllers/playlistController.js
+++ controllers/playlistController.js
@@ -7,6 +7,14 @@ exports.createPlaylist = async (req, res) => {
 };
 
 exports.getPlaylists = async (req, res) => {
+  const uid = req.query;
+  if (uid) {
+    console.log(uid.uid);
+    const ress = await Playlist.find({ description: uid.uid });
+    console.log(ress);
+    res.send(ress);
+    return;
+  }
   const result = await Playlist.find({});
   res.send(result);
 };
